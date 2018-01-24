a6b20ff0bbfa15fce6da9a2e78ec795cffa84734
zhouxun <zhouxun@xuexibao.cn>
2018-01-24 15:21:14 +0800

    remove

--- io/ekt8/db/db.go
+++ io/ekt8/db/db.go
@@ -11,33 +11,3 @@ import (
 
 type EKTDB interface {
 }
-
-func Query() {
-	db, err := sql.Open("sqlite3", "/home/x/s")
-	time.Sleep(3 * time.Second)
-	defer db.Close()
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(-1)
-	}
-	rows, err := db.Query("SELECT `id`, `name`, `password` FROM `test` WHERE `name` = ?", "zhouxun")
-	for rows.Next() {
-		var id int
-		var name, password string
-		rows.Scan(&id, &name, &password)
-		fmt.Println(id, name, password)
-	}
-}
-
-func Insert() {
-	db, err := sql.Open("sqlite3", "/home/x/s")
-	time.Sleep(3 * time.Second)
-	defer db.Close()
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(-1)
-	}
-	stmt, err := db.Prepare("INSERT INTO `test`(`name`, `password`) VALUES(?, ?)")
-	res, err := stmt.Exec("zhouxun", "helloworld")
-	fmt.Println(res.LastInsertId())
-}
--- io/ekt8/main.go
+++ io/ekt8/main.go
@@ -7,13 +7,4 @@ import (
 )
 
 func main() {
-	for i := 0; i < 10; i++ {
-		rand.Seed(time.Now().Unix())
-		if rand.Int()%2 == 1 {
-			go db.Query()
-		} else {
-			go db.Insert()
-		}
-	}
-	time.Sleep(10 * time.Second)
 }
